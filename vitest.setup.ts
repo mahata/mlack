722f311e98fde11cd1375f41c5265d0be48d6b38
Yasunori MAHATA <mahata777@gmail.com>
2026-03-14 00:10:16 +0900

    fix(env): update Google redirect URI in sample and test setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,7 +3,7 @@ beforeAll(() => {
   // Set environment variables
   process.env.GOOGLE_CLIENT_ID = "test-client-id";
   process.env.GOOGLE_CLIENT_SECRET = "test-client-secret";
-  process.env.GOOGLE_REDIRECT_URI = "http://localhost:3000/auth/google/callback";
+  process.env.GOOGLE_REDIRECT_URI = "http://localhost:3000/auth/google";
   process.env.SESSION_SECRET = "test-session-secret";
   process.env.NODE_ENV = "test";
 
